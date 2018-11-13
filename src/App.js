4639d42b55b18ec6b068686acecabb4703bc1c44
mike-carbone <mfcbone2@gmail.com>
2018-11-13 17:24:41 -0500

    :art: Implement DRY principles, clean up code

--- src/App.js
+++ src/App.js
@@ -2,36 +2,70 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      followers: '',
+      following: '',
+    };
+    
+    this.refresher = this.refresher.bind(this);
+    this.usernameEntered = this.usernameEntered.bind(this);
+  }
+
+  componentDidMount(){
+    //Check local storage
+  }
 
   usernameEntered(){
+
     let username = document.getElementById("twitter-user-js").value;
+    let followerCount;
+    
+    function readyStateCallback(req){
+      console.log('Ready state callback called');
+      console.log(req.readyState);
+      if (req.readyState === 4) {
+            var responseObject = JSON.parse(req.responseText);
+            var finishStatus = responseObject.finishedAt;
+            var resultsUrl = responseObject.resultsUrl;
+            var requestId = responseObject._id;
+            
+            //Check if results are ready to be requested
+            return (finishStatus == null) ? poller(requestId) : fetchResults(resultsUrl);
+      } else {
+        return console.log('Invalid ready state');
+      }
+    }
+
+    function fetchResults(theURL){
+          var Httpreq = new XMLHttpRequest();
+              Httpreq.open("GET",theURL,false);
+              Httpreq.send(null);
+          
+          var dataPayload = JSON.parse(Httpreq.responseText);
+          followerCount =  dataPayload[0].pageFunctionResult[2];
+          console.log('FOLLOWER COUNT: ', followerCount);
+          
+          return followerCount;
+    }
+
+    function poller(requestId){
+        
+        console.log('Original poll still runnning, executing long poller');
+        
+        setTimeout(function(){
+          var request = new XMLHttpRequest();
+              request.open('GET', `https://api.apify.com/v1/execs/${requestId}`);
+              request.onreadystatechange = function(){ readyStateCallback(this)};
+              request.send();
+
+        }, 1000);
+      }
+
+    //Validate field content
     if (username){
-      console.log(username);
-
-      var request = new XMLHttpRequest();
-
-      request.open('POST', 'https://api.apify.com/v1/F6zg3S4YnudDgw5xe/crawlers/yxCYAw3hon6qnebkN/execute?token=xSCGuWqoaXFFmfzrvNH9zW43L');
-
-      request.setRequestHeader('Content-Type', 'application/json');
-
-      request.onreadystatechange = function () {
-        if (this.readyState === 4) {
-          var responseObject = JSON.parse(this.responseText);
-          var finishStatus = responseObject.finishedAt;
-          var resultsUrl = responseObject.resultsUrl;
-          var requestId = responseObject._id;
-          console.log('requestId: ', requestId);
-          if (finishStatus == null) {
-            poller(requestId);
-          } else {
-            console.log('Status:', this.status);
-            console.log('Headers:', this.getAllResponseHeaders());
-            console.log('Body:', this.responseText);
-            console.log('Success! Finished at: ', finishStatus);
-            Get(resultsUrl);
-          }
-        }
-      };
 
       var body = {
         '_id': 'yxCYAw3hon6qnebkN',
@@ -41,61 +75,21 @@ class App extends Component {
         }]
       };
 
-      request.send(JSON.stringify(body));
-      
-      function poller(requestId){
-        console.log('Original poll still runnning, executing long poller');
-        setTimeout(function(){
-          
-          var request = new XMLHttpRequest();
-
-          request.open('GET', `https://api.apify.com/v1/execs/${requestId}`);
-
-          request.onreadystatechange = function () {
-            if (this.readyState === 4) {
-              
-              var responseObject = JSON.parse(this.responseText);
-              var finishStatus = responseObject.finishedAt;
-              var resultsUrl = responseObject.resultsUrl;
-              var requestId = responseObject._id;
-
-              if (finishStatus == null) {
-                poller(requestId);
-              } else {
-                console.log('Status:', this.status);
-                console.log('Headers:', this.getAllResponseHeaders());
-                console.log('Body:', this.responseText);
-                console.log('Success! Finished at: ', finishStatus);
-                Get(resultsUrl);
-              }
-            }
-          };
-          request.send();
-
-          
-
-        }, 1000);
-      }
-
-      function Get(theURL){
-          var Httpreq = new XMLHttpRequest(); // a new request
-          Httpreq.open("GET",theURL,false);
-          Httpreq.send(null);
-          var rawPayload = Httpreq.responseText;
-          var dataPayload = JSON.parse(rawPayload);
-          console.log('PAYLOAD: ', dataPayload);
-          console.log(`This user has ${dataPayload[0].pageFunctionResult[2]} followers`);   
-      }
-      
-         
-
+      var request = new XMLHttpRequest();
+          request.open('POST', 'https://api.apify.com/v1/F6zg3S4YnudDgw5xe/crawlers/yxCYAw3hon6qnebkN/execute?token=xSCGuWqoaXFFmfzrvNH9zW43L');
+          request.setRequestHeader('Content-Type', 'application/json');
+          request.onreadystatechange = function(){ readyStateCallback(this);};
+          request.send(JSON.stringify(body));
+  
     } else {
+      //Please enter a username!
       console.log('nothin');
     }
-    
   }
 
-
+  refresher(data){
+    this.setState({followers: data});
+  }
 
   render() {
     return (
@@ -103,6 +97,12 @@ class App extends Component {
         <h1 className="header">Socializr</h1>
         <input id="twitter-user-js" placeholder="Enter your Twitter user name here" type="text" />
         <button onClick={this.usernameEntered}>Submit</button>
+        <div className="stats">
+          <p>Followers: {this.state.followers}</p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
       </div>
     );
   }
