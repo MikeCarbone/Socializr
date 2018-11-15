76d24ae31a2aa127e2cea2393fa85986ac29a80c
mike-carbone <mfcbone2@gmail.com>
2018-11-15 01:37:39 -0500

    Updates

---
+++ precache-manifest.df179007a4f413550cc9fa5c28fb87ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3630ed5d5c50bf9f583b",
+    "url": "/socializr/static/js/runtime~main.3630ed5d.js"
+  },
+  {
+    "revision": "6a3f52e291c69e27aedf",
+    "url": "/socializr/static/js/main.6a3f52e2.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/socializr/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "6a3f52e291c69e27aedf",
+    "url": "/socializr/static/css/main.88e83585.chunk.css"
+  },
+  {
+    "revision": "0ee88a2337f3a126e4613d917cd5b577",
+    "url": "/socializr/index.html"
+  }
+];
